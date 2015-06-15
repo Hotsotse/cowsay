37ed6d1b8c9c3170b68750143a3bc0801cbc7182
Hotsotse <hotsotse1@hotmail.com>
2015-06-14 21:50:34 -0400

    woznaik basically fixed everything; changed cin to a getline.

--- cowsay.cpp
+++ cowsay.cpp
@@ -9,26 +9,46 @@
 
 using namespace std;
 
-string(int t = var.len, char c);
+string underscore_length(string var)
+{
+        string start = " _";
+        string add = "_"; // for ajusting lenth of bubble roof
+        for (int i = 0; i <= var.size(); i++)
+        {
+                start = start + add;
+        }
+        return start;
+}
 
+string hyphen_length(string var)
+{
+	string start("  -");
+	string add("-");
+	for (int i = 0; i <= var.size(); i++)
+	{
+		start = start + add;
+	}
+	return start;
+}
+ 
 void  cowsay(string var)
 {
-char star = '*';
-string hyphen(var, star);
-cout << "  ____" << endl;
-cout << " < " << var << " >" << endl; //our var
-cout << "  " << hyphen << "\n"
-"         \\   ^__^\n" //double slashes to tell gcc that we're not running wizardy commands or anythin'
-"          \\  (oo)\\_______\n"
-"             (__)\\       )\\/\\\n"
-"        	||----w |\n"
-"           	||     ||\n" << endl;
+        char star = '*';
+        cout << " "<<underscore_length(var) << endl;
+        cout << " < " << var << " >" << endl; //our var
+		cout << hyphen_length(var) << endl;
+        cout << "  " << "\n"
+                "         \\   ^__^\n" //double slashes to tell gcc that we're not running wizardy commands or anythin'
+                "          \\  (oo)\\_______\n"
+                "             (__)\\       )\\/\\\n"
+                "               ||----w |\n"
+                "               ||     ||\n" << endl;
 }
 
 int main()
 {	
 	string var;
-	cin >> var;
+	getline(cin, var);
     cowsay(var);
  	return 0;
 }
